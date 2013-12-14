c9e4296fb821e8eda6d0a5937c88c7587cbd4f4a
Islam Wazery <wazery@ubuntu.com>
2013-12-14 23:20:55 +0200

    First commit, the scaffold code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firn::Application.config.secret_token = 'bf23b300189978750c951f88d32eb9495f8da5a992e874f4dac6c1319242d23483a13f998ffcd483ac8ecd93f97f356ae31f7bf219ac8cba7a4102c83631dcbd'
