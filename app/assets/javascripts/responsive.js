7ac69b0fe951baab55454674ec565a5a1b81c86b
Islam Wazery <wazery@ubuntu.com>
2013-12-22 02:22:24 +0200

    Fixed JQuery issue on Android

--- app/assets/javascripts/responsive.js
+++ app/assets/javascripts/responsive.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-	$('#nav-menu').click(function() {
+	$('.nav-menu').click(function() {
 		if ($('.nav-list').hasClass('nav-list-open') && $('.nav-hold').hasClass('nav-hold-open')) {
 			$('.nav-list').removeClass('nav-list-open', 1000);
 			$('.nav-hold').removeClass('nav-hold-open', 1000);
