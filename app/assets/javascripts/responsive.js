7cf8e7e827546c5a876a5c2ed611d151fc8e2e4d
Islam Wazery <wazery@ubuntu.com>
2013-12-22 06:46:11 +0200

    Added flexbox to the testimonials section

--- app/assets/javascripts/responsive.js
+++ app/assets/javascripts/responsive.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
 
 	$('.nav-menu').click(function() {
 		if ($('.nav-list').hasClass('nav-list-open') && $('.nav-hold').hasClass('nav-hold-open')) {
-			$('.nav-list').removeClass('nav-list-open', 1000);
-			$('.nav-hold').removeClass('nav-hold-open', 1000);
+			$('.nav-list').removeClass('nav-list-open', 200);
+			$('.nav-hold').removeClass('nav-hold-open', 200);
 		}
 		else {
-			$('.nav-list').addClass('nav-list-open', 1000);
-			$('.nav-hold').addClass('nav-hold-open', 1000);
+			$('.nav-list').addClass('nav-list-open', 200);
+			$('.nav-hold').addClass('nav-hold-open', 200);
 		}
 	});
 });
